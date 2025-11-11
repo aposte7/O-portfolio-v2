e6d1c0ff2483c71034d1714f7d4780b2084df06c
aposte7 <olman4jesus@gmail.com>
2025-11-11 19:31:53 +0300

    refactor: update Backend title and adjust Skills component layout for better organization

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -29,7 +29,7 @@ const skillGroups = [
     ],
   },
   {
-    title: "Backend / Full-Stack",
+    title: "Backend",
     iconColor: "text-purple-500",
     skills: [
       { name: "Node.js", Icon: SiNodedotjs },
@@ -58,7 +58,7 @@ export default function Skills() {
           Tech Stack & Skills
         </h2>
 
-        <div className="mx-auto grid w-lg gap-8 md:w-full md:grid-cols-2 lg:grid-cols-4">
+        <div className="mx-auto grid w-lg gap-8 md:w-full md:grid-cols-2 lg:grid-cols-3">
           {skillGroups.map((group) => (
             <div
               key={group.title}
